da8c10d1b2624f87f7003cbfde98b8e5984e1acb
stada526 <tsria526@gmail.com>
2025-05-16 14:58:54 -0400

    Update package.json to add start script and replace NxWelcome component with Products in App component; remove unused nx-welcome.tsx file.

--- apps/react-store/src/app/app.tsx
+++ apps/react-store/src/app/app.tsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { Products } from '@react-monorepo/products'
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="react-store" />
+      <Products />
     </div>
   );
 }
